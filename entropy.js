dd7e3314d58406d2ae2edccef7ecb16681654d39
ut1Puti <91106828+ut1Puti@users.noreply.github.com>
2021-09-30 18:40:13 +0500

    Add files via upload

--- entropy.js
+++ entropy.js
@@ -1,6 +1,5 @@
 let fs = require('fs');
 let inText;
-let arg = process.argv;
 let alph = new Array();
 let entropy = 0;
 let i;
@@ -16,21 +15,14 @@ fs.readFile('abrakadabra.txt', function(error, data){
     for (i = 0; i < inText.length; i++){
 		alph[inText.charAt(i)]++;
 	}
-	while (j < inText.length){
-		for (i in alph){
-			if (i == inText.charAt(j)){
-				n +=1;
-			}
-		j +=1;
-		}
+
+	for (i in alph){
+		n += 1;
 	}
-	console.log(n);
-	console.log(alph);
+	
 	for (i in alph){
 		i = alph[i]/inText.length;
-		console.log(i);
-		entropy += i*(Math.log(i)/Math.log(n)); 
+		entropy -= i*(Math.log(i)/Math.log(n)); 
 	}
-	entropy = -entropy;
 	console.log(entropy);
 });
