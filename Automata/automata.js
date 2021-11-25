d22bb3964dfd45fa453af3492fa0d793cf2b2c0b
ut1Puti <91106828+ut1Puti@users.noreply.github.com>
2021-11-25 15:50:39 +0500

    Add files via upload

---
+++ Automata/automata.js
@@ -0,0 +1,50 @@
+let arg = process.argv;
+let str = arg[2].toString();
+let strLen = str.length;
+let key = arg[3].toString();
+let keyLen = key.length;
+let alph = new Array();
+let del = new Array(strLen + 1);
+let result = new Array();
+let flag, currlen;
+
+for (i = 0; i < keyLen; i++){
+    alph[key.charAt(i)] = 0; 
+}
+console.log(alph);
+
+for (i = 0; i <= keyLen; i++){
+    del[i] = new Array();
+}
+for (i in alph){
+    del[0][i] = 0;
+}
+for (i = 0; i < keyLen; i++){
+    let prev = del[i][key.charAt(i)];
+    del[i][key.charAt(i)] = i + 1;
+    for (j in alph){
+        del[i + 1][j] = del[prev][j];
+    }
+}
+console.log(del);
+
+for (let i = 0; i < strLen; i++){
+    flag = false;
+    for (j in alph){
+        if (j == str.charAt(i)){
+            currLen = del[currLen][str.charAt(i)];
+            if (currLen == keyLen){
+                result.push(i - keyLen + 1);
+            }
+            flag = true;
+            break;
+        }
+    }
+    if (flag == false){
+        currLen = 0;
+    }
+}
+console.log(result.join(', '));
+
+
+// node automata.js аnananaananas ananas
